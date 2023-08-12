99be827a887237dbef1371f099f372fa17001499
huynhhoc <huynhhoc@gmail.com>
2023-08-12 16:10:41 +0200

    update index

--- wwwroot/js/map.js
+++ wwwroot/js/map.js
@@ -1,4 +1,3 @@
-// map.js
 var map = L.map('map').setView([0, 0], 2);
 
 var wmsLayer = L.tileLayer.wms('http://localhost:8080/geoserver/ne/wms', {
@@ -9,12 +8,32 @@ var wmsLayer = L.tileLayer.wms('http://localhost:8080/geoserver/ne/wms', {
     attribution: "World boundaries &copy; <a href='http://www.naturalearthdata.com'>Natural Earth</a>"
 }).addTo(map);
 
-var infoControl = L.control.wmsFeatureInfo({
-    autoActivate: true,
-    content: 'Loading...',
-    layers: [wmsLayer],
-    queryVisible: true,
-    infoFormat: 'text/html'
-});
+var wmsLayerfr = L.tileLayer.wms('http://localhost:8080/geoserver/ne/wms', {
+    layers: 'ne:FRA_adm1',
+    format: 'image/png',
+    transparent: true,
+    version: '1.1.0',
+    attribution: "World boundaries &copy; <a href='http://www.naturalearthdata.com'>Natural Earth</a>",
+    style: function (feature) {
+        var name = feature.properties.name_1;
+        // Define a color palette based on name_1 attribute
+        var color = getColorBasedOnName(name);
+        return {
+            fillColor: color,
+            color: '#5733FF',
+            weight: 1,
+            fillOpacity: 0.7
+        };
+    }
+}).addTo(map);
+function getColorBasedOnName(name) {
+    console.log("Checking name:", name);
+    var colorPalette = {
+        "Alsace": "#FF5733",
+        "Aquitaine": "#33FF57",
+        "Auvergne": "#5733FF"
+    };
 
-map.addControl(infoControl);
+    var defaultColor = "#999999"; // Default color if name is not found
+    return colorPalette[name] || defaultColor;
+}
